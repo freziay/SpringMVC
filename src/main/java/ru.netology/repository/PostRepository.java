1a2e0cd6c449c66a26c699444e149f6b00e73ffa
Nastena <freziay@mail.ru>
2023-07-04 22:49:42 +0300

    Добавила ApplicationInitializer.java и аннотацию в метод контроллер removeById()1.

--- src/main/java/ru.netology/repository/PostRepository.java
+++ src/main/java/ru.netology/repository/PostRepository.java
@@ -1,16 +1,41 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
-public interface PostRepository {
-    List<Post> all();
+// Stub
+public class PostRepository {
 
-    Optional<Post> getById(long id);
+    private final AtomicLong atomicLong = new AtomicLong(0);
 
-    Post save(Post post);
+    private final Map<Long, Post> posts = new HashMap<>();
 
-    void removeById(long id);
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long id = atomicLong.addAndGet(1);
+            post.setId(id);
+            posts.put(id, post);
+            return posts.get(id);
+        } else if (posts.get(post.getId()) == null) {
+            throw new NotFoundException();
+        } else {
+            posts.replace(post.getId(), post);
+            return posts.get(post.getId());
+        }
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
